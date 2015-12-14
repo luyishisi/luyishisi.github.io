e75f71c2ccf6f55434fefe0ef66ce25b97d7e0dc
luyishisi <543429245@qq.com>
2015-12-14 14:34:43 +0800

    meishi

---
+++ game2/game1/js/js-one.js
@@ -0,0 +1,10 @@
+/**
+ * Created by luyi on 2015/7/23.
+ */
+$(document).ready(function()
+{
+    $("button").click(function(){
+        $(".head").css("color","red")
+            .slideToggle(2000)
+    });
+});
